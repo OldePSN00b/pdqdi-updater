54c428d60a82f66a54d13886031f384152a08d19
OldePSN00b <terrigan@gmail.com>
2024-11-07 10:08:59 -0600

    Update UpdatePDQProducts.ps1
    Added the #requies thanks to @andrewplatech

--- UpdatePDQProducts.ps1
+++ UpdatePDQProducts.ps1
@@ -1,5 +1,6 @@
 #Author: OldePSN00b
 #Date: 2024-09-10
+#Requires -Modules @{ ModuleName="PdqStuff"; RequiredVersion="6.1.1" } 
 
 #What am I doing? Can I write a module? Heck, I don't know, I'm a N00b so I'll start with a script. So this requires Colby Burma's PDQStuff Module 
 #(https://gitlab.com/ColbyBouma/pdqstuff), so it would need to be installed first. Ok so what do I want this to do? Well, basically update PDQ Inventory
