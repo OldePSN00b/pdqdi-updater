a5f58f0ff644191897eb89fbdb86ea91bfb4745f
OldePSN00b <terrigan@gmail.com>
2024-11-07 10:09:42 -0600

    Update UpdatePDQProducts.ps1
    Changed date

--- UpdatePDQProducts.ps1
+++ UpdatePDQProducts.ps1
@@ -1,5 +1,5 @@
 #Author: OldePSN00b
-#Date: 2024-09-10
+#Date: 2024-11-07
 #Requires -Modules @{ ModuleName="PdqStuff"; RequiredVersion="6.1.1" } 
 
 #What am I doing? Can I write a module? Heck, I don't know, I'm a N00b so I'll start with a script. So this requires Colby Burma's PDQStuff Module 
